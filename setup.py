6c21b000d2ce3b12a52dedc011fa4f34cbd59a27
Josh Hudson <jhudson@directemployers.org>
2012-01-13 11:54:49 -0500

    Add version number to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-celery-haystack-index',
-    packages=find_packages(),
-    include_package_data=True,
-    description='Celery Haystack SearchIndex',
-    long_description=open('README.md').read(),
-    zip_safe=False, # because we're including media that Django needs
+    name = 'django-celery-haystack-index',
+    version = '0.1',
+    packages = find_packages(),
+    include_package_data = True,
+    description = 'Celery Haystack SearchIndex',
+    long_description = open('README.md').read(),
+    zip_safe = False, # because we're including media that Django needs
 )
 

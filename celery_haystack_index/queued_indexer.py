0618a1cd097dd091b807c025b358beea1c1694f8
Josh Hudson <jhudson@directemployers.org>
2012-01-18 12:51:03 -0500

    Add dispatch_uid to signal connectors to prevent duplicate signal bindings

--- celery_haystack_index/queued_indexer.py
+++ celery_haystack_index/queued_indexer.py
@@ -9,11 +9,20 @@ from index_tasks import search_index_update, search_index_delete
 class QueuedSearchIndex(indexes.SearchIndex):
     """A ``SearchIndex`` subclass that enqueues updates for later processing."""
     # We override the built-in _setup_* methods to connect the enqueuing operation.
+
+    def assemble_dispatch_uid(self, method):
+        return '.'.join([self.__module__, self.__class__.__name__,
+                        method.__func__.__name__])
+
     def _setup_save(self):
-        signals.post_save.connect(self.enqueue_save, sender=self.get_model())
+        signals.post_save.connect(
+            self.enqueue_save, sender=self.get_model(),
+            dispatch_uid=self.assemble_dispatch_uid(self._setup_save))
 
     def _setup_delete(self):
-        signals.post_delete.connect(self.enqueue_delete, sender=self.get_model())
+        signals.post_delete.connect(
+            self.enqueue_delete, sender=self.get_model(),
+            dispatch_uid=self.assemble_dispatch_uid(self._setup_delete))
 
     def _teardown_save(self):
         signals.post_save.disconnect(self.enqueue_save, sender=self.get_model())

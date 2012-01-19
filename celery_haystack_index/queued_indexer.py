c9cfe12d453edc8fe165f6dc70e8438d27a76a93
Josh Hudson <jhudson@directemployers.org>
2012-01-19 11:06:27 -0500

    Add BulkQueuedSearch index, tasks, and signals to handle bulk update and bulk delete to solr through haystack
    Fix typo in signal declarations
    
    Add conditions to enqueue_bulk_change to only send if you have items to update/delete (duh)
    
    Revert the name of setup and teardown hooks on BulkQueuedSearchIndex
    
    Forgot to import bulk tasks
    
    Stupid mistake forgetting to wrap multi line import with parens

--- celery_haystack_index/queued_indexer.py
+++ celery_haystack_index/queued_indexer.py
@@ -1,9 +1,11 @@
 from django.db.models import signals
+from celery_haystack_index.signals import solr_bulk_change
 
 from haystack import indexes
 from haystack.utils import get_identifier
 
-from index_tasks import search_index_update, search_index_delete
+from celery_haystack_index.index_tasks import (search_index_update,
+search_index_delete, search_index_bulk_update, search_index_bulk_delete)
 
 
 class QueuedSearchIndex(indexes.SearchIndex):
@@ -38,3 +40,27 @@ class QueuedSearchIndex(indexes.SearchIndex):
 
     def enqueue_delete(self, instance, **kwargs):
         search_index_delete.delay(instance)
+
+class BulkQueuedSearchIndex(indexes.SearchIndex):
+    """A ``SearchIndex`` subclass that enqueues updates for later processing."""
+    # We override the built-in _setup_* methods to connect the enqueuing
+    # operation.
+
+    def assemble_dispatch_uid(self, method):
+        return '.'.join([self.__module__, self.__class__.__name__,
+                        method.__func__.__name__])
+
+    def _setup_save(self):
+        solr_bulk_change.connect(
+            self.enqueue_bulk_change, 
+            dispatch_uid=self.assemble_dispatch_uid(self._setup_save))
+
+    def _teardown_save(self):
+        solr_bulk_change.disconnect(self.enqueue_bulk_change)
+
+    def enqueue_bulk_change(self, update_items=None, delete_query=None, **kwargs):
+        if update_items:
+            search_index_bulk_update.delay(update_items)
+        if delete_query:
+            search_index_bulk_delete.delay(delete_query)
+

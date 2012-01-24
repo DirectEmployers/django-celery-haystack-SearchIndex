96b2d31c4fd44da67d56210bc4265f2974f55b9b
Josh Hudson <jjhudson1@gmail.com>
2012-01-24 12:28:50 -0500

    Forgot to import bulk tasks

--- celery_haystack_index/queued_indexer.py
+++ celery_haystack_index/queued_indexer.py
@@ -4,7 +4,8 @@ from celery_haystack_index.signals import solr_bulk_change
 from haystack import indexes
 from haystack.utils import get_identifier
 
-from celery_haystack_index.index_tasks import search_index_update, search_index_delete
+from celery_haystack_index.index_tasks import search_index_update,
+search_index_delete, search_index_bulk_update, search_index_bulk_delete
 
 
 class QueuedSearchIndex(indexes.SearchIndex):
